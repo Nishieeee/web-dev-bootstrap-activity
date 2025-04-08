414449bb6e0dd50abe3dcf789612a9b526a1e4b8
Nishieeee <148634186+Nishieeee@users.noreply.github.com>
2025-04-08 12:01:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.querySelector("#calculate").onclick = () => {
+    let x = document.querySelector("#input-x").value;
+    let y = document.querySelector("#input-y").value;
+
+    let op = document.querySelector("#operation").value;
+
+    let total = document.querySelector("#total");
+
+    x = parseInt(x);
+    y = parseInt(y);
+
+    if(isNaN(x) || isNaN(y)) {
+        alert("Field Should not be empty");
+    } else {
+        switch(op) {
+            case '+':
+    
+                let sum = x + y;
+    
+                total.innerHTML = `Results: ${sum}`;
+                break;
+    
+            case '-':
+                let diff = x - y;
+    
+                total.innerHTML = `Results: ${diff}`;
+                break;
+    
+            case '*':
+                let prod = x * y;
+    
+                total.innerHTML = `Results: ${prod}`;
+                break;
+            
+            case '/':
+                let quo = x / y;
+    
+                total.innerHTML = `Results: ${quo}`;
+                break;
+        }
+    }
+    
+
+}
